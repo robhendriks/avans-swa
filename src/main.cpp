60b58ae7397aedd25ef5c1cfb0955b7b1cf3093d
Leendert\Leendert <lcmechie@avans.nl>
2016-10-11 14:56:57 +0200

    :neckbeard:

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
-#include <iostream>
-#include "SDL.h"
+#include <stdio.h>
+#include "SDL2/SDL.h"
 
-const int WIDTH = 640;
-const int HEIGHT = 480;
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 SDL_Window *window;
+SDL_Renderer *renderer;
 
-int main(int argc, const char *argv[]) {
+int main (int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+        fprintf(stderr, "Failed to initialize SDL: \n", SDL_GetError());
         return 1;
     }
 
-    window = SDL_CreateWindow("City Defence", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("City Defence", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
-        fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
+        fprintf(stderr, "Failed to create window: \n", SDL_GetError());
         return 1;
     }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Failed to create renderer: \n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0.0f, 0.0f, 0.0f, 1.0f);
 
     bool running = true;
 
@@ -27,8 +36,12 @@ int main(int argc, const char *argv[]) {
                 running = false;
             }
         }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
