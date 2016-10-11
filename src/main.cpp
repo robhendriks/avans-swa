b16025a4b587f6d6ef01837c0cdee23ba9918e8b
robhendriks <cyanidenl@icloud.com>
2016-10-11 18:13:49 +0200

    created asset pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <stdio.h>
-#include "SDL2/SDL.h"
-
-const int WIDTH = 800;
-const int HEIGHT = 600;
+#include <iostream>
+#include "Simpleini.h"
+#include "SDL.h"
 
 SDL_Window *window;
 SDL_Renderer *renderer;
 
 int main(int argc, char *argv[]) {
+    CSimpleIniA ini;
+    ini.SetUnicode();
+    ini.LoadFile("config.ini");
+
+    long width = ini.GetLongValue("window", "width", 800);
+    long height = ini.GetLongValue("window", "height", 600);
+
+    std::cout << width << "x" << height << std::endl;
+
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Failed to initialize SDL: \n", SDL_GetError());
         return 1;
     }
 
-    window = SDL_CreateWindow("City Defence", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
-                              SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("City Defence", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
     if (!window) {
         fprintf(stderr, "Failed to create window: \n", SDL_GetError());
         return 1;
