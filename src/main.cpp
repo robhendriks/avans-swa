e719be1a5336c42c4953c09f6ffe305bcf085870
Jermey <jermeyjungbeker2@gmail.com>
2016-10-25 15:41:41 +0200

    last edit now it works

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,8 @@
 int main(int argc, char *argv[]) {
     game::engine *engine = &game::engine::get();
     engine->run();
-    gui::menu_gui *menu = new gui::menu_gui();
-    engine->setActiveGUI(menu);
+    //gui::menu_gui *menu = new gui::menu_gui();
+    //engine->setActiveGUI(menu);
 
     return 0;
 }
-
-/*
-void engine::setActiveGUI(gui::base_gui* gui) {
-    m_gui = gui;
-}
-
-if(this->m_gui != nullptr)
-this->m_gui->render(m_display->get_renderer());
- */
