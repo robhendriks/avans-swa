71c3a000eaadf0960c7ef14e293f6535514cf6ce
Robbie op de Weegh <robbieopdeweegh@gmail.com>
2016-11-06 16:51:55 +0100

    Removed test string

--- src/engine/eventbus/eventbus.h
+++ src/engine/eventbus/eventbus.h
@@ -24,7 +24,6 @@ namespace engine {
 
             template<class T>
             void fire(T &event) {
-                std::string test = typeid(T).name();
                 for (auto &sub : subscribers[typeid(T).name()]) {
                     sub.as<subscriber<T>*>()->on_event(event);
                 }
