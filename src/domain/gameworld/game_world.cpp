92f949e8b0499e333bb449249147f4b220c81061
Jermey <jermeyjungbeker2@gmail.com>
2016-11-16 19:14:26 +0100

    no message

--- src/domain/gameworld/game_world.cpp
+++ src/domain/gameworld/game_world.cpp
@@ -5,13 +5,13 @@
 
 namespace domain {
     namespace gameworld{
-        game_world::game_world(std::vector<map::base_map*>& map) :_map(map){
+        game_world::game_world(std::vector<map::base_map*> maps) :_maps(maps){
             _current_lvl = 0;
-            auto m = map;
+            auto m = maps;
             domain::map::map* r = (domain::map::map*) m.at(0);
             r->get_tile_height();
 
-            auto s = _map;
+            auto s = _maps;
             domain::map::map* d = (domain::map::map*) s.at(0);
             d->get_tile_height();
         }
@@ -20,26 +20,26 @@ namespace domain {
 
         }
         game_world::~game_world() {
-            for(map::base_map* map : this->_map)
+            for(map::base_map* map : this->_maps)
                 delete map;
         }
 
         void game_world::draw(engine::graphics::texture_manager &texture_manager, engine::math::box2_t &dest) {
-            this->_map.at((unsigned int) this->_current_lvl)->draw(texture_manager, dest);
+            this->_maps.at((unsigned int) this->_current_lvl)->draw(texture_manager, dest);
         }
 
         void game_world::unload(engine::graphics::texture_manager &texture_manager) {
-            for(auto &m : this->_map)
+            for(auto &m : this->_maps)
                 m->unload(texture_manager);
         }
 
         map::base_map* game_world::get_current_map() {
-            auto*  t = this->_map.at(this->_current_lvl);
+            auto*  t = this->_maps.at((unsigned int) this->_current_lvl);
             domain::map::map* m = (domain::map::map*) t;
             auto r = m->get_fields();
             int s = r.size();
             s++;
-            return this->_map.at((unsigned int) this->_current_lvl);
+            return this->_maps.at((unsigned int) this->_current_lvl);
         }
 
         int game_world::get_current_lvl() {
@@ -55,7 +55,13 @@ namespace domain {
             return this->_current_lvl;
         }
 
-
+        game_world &game_world::operator=(game_world obj) {
+            this->_current_lvl = obj._current_lvl;
+            this->_maps = obj._maps;
+            domain::map::map* d = (domain::map::map*)this->_maps.at(0);
+            d->get_tile_height();
+            return *this;
+        }
     }
 }
 #endif //CITY_DEFENCE_GAME_WORLD_CPP
