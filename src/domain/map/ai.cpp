b26363548ae27adaa9061130e7abca3dca53deb0
galderli <galderli@avans.nl>
2016-12-07 17:11:17 +0100

    ai for now

---
+++ src/domain/map/ai.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bert on 7-12-2016.
+//
+
+#include "ai.h"
---
+++ src/domain/map/ai.h
@@ -0,0 +1,14 @@
+//
+// Created by Bert on 7-12-2016.
+//
+
+#ifndef CITY_DEFENCE_AI_H
+#define CITY_DEFENCE_AI_H
+
+
+class ai {
+
+};
+
+
+#endif //CITY_DEFENCE_AI_H
