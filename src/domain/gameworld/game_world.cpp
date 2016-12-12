3f26ba79d467f750e4e6b67972a17807b79d632b
Jermey <jermeyjungbeker2@gmail.com>
2016-12-12 11:51:50 +0100

    changed holding the stats in game_world to also now know what lvl its from by returning and holding a struct

--- src/domain/gameworld/game_world.cpp
+++ src/domain/gameworld/game_world.cpp
@@ -13,11 +13,18 @@ namespace domain {
         }
 
         void game_world::set_current_level(std::unique_ptr<game_level::game_level> game_lvl, bool tranfer_stats){
-            if(tranfer_stats) m_all_stats.push_back(m_level->get_stats());
+            if(tranfer_stats){
+                lvl_id_stats_and_game_stats s;
+                s.id = m_level->get_id();
+                s.name = m_level->get_name();
+                s.stat = m_level->get_stats();
+                m_all_stats.push_back(s);
+            }
+
             m_level = std::move(game_lvl);
         }
 
-        std::vector<std::shared_ptr<game_level::game_stats>> game_world::get_stats_of_previous_lvls() {
+        std::vector<lvl_id_stats_and_game_stats> game_world::get_stats_of_previous_lvls() {
             return m_all_stats;
         }
 
