e26575b59c28a50ca1cfcd90f58261307caf5656
galderli <galderli@avans.nl>
2017-01-16 21:45:18 +0100

    Mayor on the right place, ugly design

--- src/gui/views/mayor_view.cpp
+++ src/gui/views/mayor_view.cpp
@@ -17,7 +17,7 @@ void gui::views::mayor_view::before() {
     m_draw_managers.texture_manager.load_text(mayor_name, {255, 255, 255},
                                               *m_draw_managers.font_manager.get_font("roboto", 25), "mayor_text");
 
-    m_draw_managers.texture_manager.load_text(m_current_response, {255, 255, 255},
+     m_draw_managers.texture_manager.load_text(m_current_response, {255, 255, 255},
                                               *m_draw_managers.font_manager.get_font("roboto", 14), "mayor_text", 150);
 }
 
@@ -28,15 +28,22 @@ void gui::views::mayor_view::on_display_change(engine::math::box2_t display_box)
 }
 
 void gui::views::mayor_view::draw(unsigned int time_elapsed, engine::math::box2_t display_box) {
-    auto margin = 1220;
+    //current response renewal
+    m_current_display_time = time_elapsed - m_last_transition_time;
+    if (m_current_display_time > m_max_display_time) {
+        m_current_response = m_mayor->get_fifo_milestone_response();
+        m_last_transition_time = time_elapsed;
+    }
+
+    engine::math::vec2_t margin = {display_box.max.x - m_mayor_box->size().x - 20, display_box.max.y - m_mayor_box->size().y - 100};
     auto mayor_img_size = m_draw_managers.texture_manager.get_size("img_mayor_bart");
     auto mayor_text_size = m_draw_managers.texture_manager.get_size("mayor_text");
     engine::graphics::box_builder builder(mayor_img_size);
     engine::graphics::box_builder builder1(mayor_text_size);
     builder.as_left_top(m_mayor_box->left_top()).center_vertical(m_mayor_box->min.y, m_mayor_box->max.y)
-            .add_margin({margin, 200});
+            .add_margin(margin);
     builder1.as_left_top(m_mayor_box->left_top()).center_vertical(m_mayor_box->min.y, m_mayor_box->max.y)
-            .add_margin({margin, 200});
+            .add_margin({margin.x, margin.y + 200});
 
     m_draw_managers.texture_manager.draw("img_mayor_bart", builder.build());
     m_draw_managers.texture_manager.draw("mayor_text", builder1.build());
